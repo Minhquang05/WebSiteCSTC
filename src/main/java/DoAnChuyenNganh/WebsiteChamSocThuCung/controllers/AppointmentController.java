7951ac9e6ea7a8c956720c8b4434ee12cfa7ba3b
Nguyen Quang Trai <nguyenquangtrai.100203@gmail.com>
2024-12-01 13:50:34 +0700

    update datlich

--- src/main/java/DoAnChuyenNganh/WebsiteChamSocThuCung/controllers/AppointmentController.java
+++ src/main/java/DoAnChuyenNganh/WebsiteChamSocThuCung/controllers/AppointmentController.java
@@ -2,14 +2,21 @@ package DoAnChuyenNganh.WebsiteChamSocThuCung.controllers;
 
 import DoAnChuyenNganh.WebsiteChamSocThuCung.models.Appointment;
 import DoAnChuyenNganh.WebsiteChamSocThuCung.models.Doctor;
+import DoAnChuyenNganh.WebsiteChamSocThuCung.models.WorkHour;
 import DoAnChuyenNganh.WebsiteChamSocThuCung.services.AppointmentService;
 import DoAnChuyenNganh.WebsiteChamSocThuCung.services.DoctorService;
+import DoAnChuyenNganh.WebsiteChamSocThuCung.services.WorkHourService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 @Controller
@@ -21,6 +28,9 @@ public class AppointmentController {
 
     @Autowired
     private DoctorService doctorService;
+
+    @Autowired
+    private WorkHourService workHourService;
 
     @GetMapping
     public String showAppointmentList(Model model) {
@@ -36,25 +46,23 @@ public class AppointmentController {
         model.addAttribute("doctors", doctorService.getAllDoctors()); // Lấy danh sách bác sĩ thú y
         return "appointments/create-appointment";  // Trang form đặt lịch khám
     }
-
     @PostMapping("/create")
-    public String createAppointment(@RequestBody Appointment appointment) {
-        appointmentService.createAppointment(appointment);
-        return "redirect:/appointments";
+    public String createAppointment( Appointment appointment, @RequestParam("apntDate") String date, @RequestParam("availableTime") String availableTime) {
+        try{
+            DateFormat df = new SimpleDateFormat("yyyy-MM-dd hh:mm");
+            WorkHour workHour = workHourService.getWorkHourById(Long.valueOf(availableTime))
+                    .orElseThrow(() -> new IllegalArgumentException("Invalid product Id:" + availableTime));;
+            date = date+ " "+workHour.getStartTime().replace("h",":00") ;
+            System.out.println("Appointment date: "+ date);
+            appointment.setAppointmentDate(df.parse(date));
+            appointment.setAppointmentState(0);
+            appointmentService.createAppointment(appointment);
+            return "redirect:/appointments";
+        }catch(ParseException e){
+            e.printStackTrace();
+        }
+        return "appointments/create-appointment";
     }
-
-//    @GetMapping("/list")
-//    public String listAppointmentForm(Model model) {
-//        model.addAttribute("appointment", new Appointment());
-//        model.addAttribute("doctors", doctorService.getAllDoctors()); // Lấy danh sách bác sĩ thú y
-//        return "appointments/appointment-list";  //
-//    }
-//
-//    @PostMapping("/list")
-//    public String listAppointment(@RequestBody Appointment appointment) {
-//        appointmentService.createAppointment(appointment);
-//        return "redirect:/appointments";
-//    }
 
     // Trang thành công
     @GetMapping("/success")
